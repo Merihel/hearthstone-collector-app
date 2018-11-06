7334e4356efb0de1a7dc14d8614d3bae674bd581
Nils Wilmet <n.wilmet@agiirnetwork.com>
2018-11-06 12:07:46 +0100

    Rebased to Unique Objects instead of lists for some

--- app/src/main/java/com/example/lpiem/hearthstonecollectorapp/Manager/APIInterface.java
+++ app/src/main/java/com/example/lpiem/hearthstonecollectorapp/Manager/APIInterface.java
@@ -9,12 +9,17 @@ import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.PUT;
 import retrofit2.http.Path;
 
 public interface APIInterface {
+
+    // NOTE SUR LES CALLS
+    // Quand on fait un GET, l'API Symfony va renvoyer un Array, même s'il n'y a qu'un seul résultat. On passe donc des List d'objets au lieu des objets eux-mêmes.
+
     // CARDS //
     @GET("card/select/{id}")
-    Call<List<Card>> getCard( @Path("id") int id);
+    Call<Card> getCard( @Path("id") int id);
 
     @GET("card/select/{set}")
     Call<List<Card>> getCardBySet( @Path("set") String set);
@@ -29,7 +34,7 @@ public interface APIInterface {
     Call<List<Card>> getCardByFaction( @Path("faction") String faction);
 
     @POST("card/new")
-    Call<List<Card>> createCard(@Body Card card);
+    Call<Card> createCard(@Body Card card);
 
 
 
@@ -37,10 +42,10 @@ public interface APIInterface {
     @GET("/user/select/{id}")
     Call<User> getUser(@Path("id") int id);
 
-    @GET("user/new")
+    @POST("user/new")
     Call<User> createUser(@Body User user);
 
-    @GET("/user/update")
-    Call<User> getUserWithCards(@Body User user);
+    @POST("/user/update")
+    Call<User> updateUser(@Body User user);
 
 }
