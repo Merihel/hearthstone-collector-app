e63eff13e14cf729a7ec144b8f3e0ad9a85a31d0
Unknown <morgane0798@hotmail.fr>
2018-12-11 17:40:16 +0100

    [WIP] Fragment listeDecks (recyclerView)

--- app/src/main/java/com/example/lpiem/hearthstonecollectorapp/Fragments/DecksListFragment.kt
+++ app/src/main/java/com/example/lpiem/hearthstonecollectorapp/Fragments/DecksListFragment.kt
@@ -6,17 +6,26 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.lpiem.hearthstonecollectorapp.Interface.InterfaceCallBackCard
 import com.example.lpiem.hearthstonecollectorapp.Interface.InterfaceCallBackDeck
 import com.example.lpiem.hearthstonecollectorapp.Interface.InterfaceCallBackUser
 import com.example.lpiem.hearthstonecollectorapp.Manager.APIManager
+import com.example.lpiem.hearthstonecollectorapp.Models.Card
+import com.example.lpiem.hearthstonecollectorapp.Models.Deck
+import com.example.lpiem.hearthstonecollectorapp.Models.User
 
 import com.example.lpiem.hearthstonecollectorapp.R
 
 private var rootView: View? = null
 private var llayout: LinearLayout? = null
 
-class DecksListFragment : androidx.fragment.app.Fragment() {
+class DecksListFragment : androidx.fragment.app.Fragment(), InterfaceCallBackDeck, InterfaceCallBackUser, InterfaceCallBackCard {
+
+    private lateinit var recyclerView: RecyclerView
+    private var lManager: LinearLayoutManager? = null
+    private lateinit var viewAdapter: RecyclerView.Adapter<*>
 
     companion object {
         fun newInstance(): CardsListFragment {
@@ -33,29 +42,25 @@ class DecksListFragment : androidx.fragment.app.Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         val controller = APIManager(this as InterfaceCallBackDeck, this as InterfaceCallBackUser, this as InterfaceCallBackCard)
         //controller.getDecksByUser(1)
     }
-//
-//    override fun onWorkCardDone(result: List<Card>) {
-//
-//    }
-//
-//    override fun onWorkCardsDone(result: List<Card>) {
-//        System.out.println("My user cards" + result.toString())
-//        llayout = rootView?.findViewById(R.id.cardsListLLayout)
-//        result.forEach {
-//            var dynamicTextView = TextView(context)
-//            dynamicTextView.text = it.name
-//            llayout?.addView(dynamicTextView)
-//        }
-//        //Need to push the cards to the RecyclerView
-//    }
-//
-//    override fun onWorkUserDone(result: List<User>) {
-//
-//    }
+
+    override fun onWorkDeckDone(result: List<Deck>) {
+        System.out.println("My user decks" + result.toString())
+
+//        rv_cards_list.adapter = DecksListAdapter(result, getActivity()!!.applicationContext)
+//        rv_cards_list.layoutManager = LinearLayoutManager(context, 3)
+    }
+
+    override fun onWorkUserDone(result: List<User>) {
+    }
+
+    override fun onWorkCardDone(result: List<Card>) {
+    }
+
+    override fun onWorkCardsDone(result: List<Card>) {
+    }
 
 
 }
